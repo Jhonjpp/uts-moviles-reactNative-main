fa4f77660730ee12ad177c39d584085c26050ce5
Jhonjpp <91070857+Jhonjpp@users.noreply.github.com>
2023-08-22 21:13:44 -0500

    clase3

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
-import Calculadora from "./components/Calculadora";
-import SumarArray from "./components/SumarArray";
-import EvaluarArray from './components/EvaluarArray';
-import AgregarNumsMayores10 from "./components/AggregarNumsArr";
-import MostrarDatosMySQL from "./components/MostrarDatosMySQL";
+import Calculadora from "./src/components/Calculadora.jsx";
+import SumarArray from "./src/components/SumarArray";
+import EvaluarArray from './src/components/EvaluarArray';
+import AgregarNumsMayores10 from "./src/components/AggregarNumsArr";
+import MostrarDatosMySQL from "./src/components/MostrarDatosMySQL";
 
 
 
@@ -13,7 +13,7 @@ export default function App() {
   
   return (
     <View style={estilos.container}>
-      <MostrarDatosMySQL/>
+      <Calculadora/>
       <StatusBar style="auto" />
     </View>
   );
